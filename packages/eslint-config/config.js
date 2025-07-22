9d7c6d7e486ecacab5062fa6ec01d4ca3f1e431e
Simon Stranks <sstranks87@gmail.com>
2025-07-22 13:25:51 +0100

    chore(fem): final push from Ubuntu distro

--- packages/eslint-config/config.js
+++ packages/eslint-config/config.js
@@ -1,19 +1,20 @@
-import ParserTypescriptEslint from "@typescript-eslint/parser";
-import PluginTypescriptEslint from "@typescript-eslint/eslint-plugin";
-import PluginUnicorn from "eslint-plugin-unicorn";
-import PluginPrettier from "eslint-plugin-prettier";
-import PluginImport from "eslint-plugin-import";
-import PluginJest from "eslint-plugin-jest";
+import ParserTypescriptEslint from '@typescript-eslint/parser';
+import PluginTypescriptEslint from '@typescript-eslint/eslint-plugin';
+import PluginUnicorn from 'eslint-plugin-unicorn';
+import PluginPrettier from 'eslint-plugin-prettier';
+import PluginImport from 'eslint-plugin-import';
+import PluginJest from 'eslint-plugin-jest';
 
-import RecommendedEslint from "@eslint/js";
-import globals from "globals";
+import RecommendedEslint from '@eslint/js';
+import globals from 'globals';
 
-export { default as ConfigPrettier } from "eslint-config-prettier";
+export { default as ConfigPrettier } from 'eslint-config-prettier';
 
 export const EslintConfig = {
+  ...PluginTypescriptEslint.configs.recommended,
   languageOptions: {
     ecmaVersion: 2021,
-    sourceType: "module",
+    sourceType: 'module',
     globals: {
       ...globals.es2021,
     },
@@ -24,7 +25,7 @@ export const EslintConfig = {
     },
   },
   plugins: {
-    "@typescript-eslint": PluginTypescriptEslint,
+    '@typescript-eslint': PluginTypescriptEslint,
     unicorn: PluginUnicorn,
     import: PluginImport,
     jest: PluginJest,
@@ -33,51 +34,43 @@ export const EslintConfig = {
   rules: {
     ...RecommendedEslint.configs.recommended.rules,
     ...PluginUnicorn.configs.recommended.rules,
-    ...PluginTypescriptEslint.configs.recommended.rules,
     ...PluginJest.configs.recommended.rules,
-    "arrow-body-style": "off",
-    "no-unused-vars": "off",
-    "no-undef": "off",
-    "no-console": "off",
-    "no-underscore-dangle": "off",
-    "no-use-before-define": "off",
-    "import/no-unresolved": "error",
-    "prettier/prettier": ["error"],
-    "@typescript-eslint/no-unused-vars": "error",
-    "@typescript-eslint/no-use-before-define": "error",
-    "unicorn/prefer-module": "off",
-    "unicorn/expiring-todo-comments": "off",
-    "unicorn/prevent-abbreviations": "off",
-    "unicorn/no-array-for-each": "off",
-    "unicorn/no-array-reduce": "off",
-    "unicorn/filename-case": [
-      "error",
+    'arrow-body-style': 'off',
+    'no-unused-vars': 'off',
+    'no-undef': 'off',
+    'no-console': 'off',
+    'no-underscore-dangle': 'off',
+    'no-use-before-define': 'off',
+    'import/no-unresolved': 'error',
+    'prettier/prettier': ['error'],
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-use-before-define': 'error',
+    'unicorn/prefer-module': 'off',
+    'unicorn/expiring-todo-comments': 'off',
+    'unicorn/prevent-abbreviations': 'off',
+    'unicorn/no-array-for-each': 'off',
+    'unicorn/no-array-reduce': 'off',
+    'unicorn/filename-case': [
+      'error',
       {
         cases: {
           camelCase: true,
           pascalCase: true,
         },
-        ignore: [
-          "index.(js|jsx|ts|tsx)",
-          "webpack.*",
-          ".d.ts",
-          "types.ts",
-          ".test.ts",
-          "use\\w*.tsx",
-        ],
+        ignore: ['index.(js|jsx|ts|tsx)', 'webpack.*', '.d.ts', 'types.ts', '.test.ts', 'use\\w*.tsx'],
       },
     ],
   },
   settings: {
-    "import/parsers": {
-      "@typescript-eslint/parser": ["*.ts", "*.tsx", "*.html"],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['*.ts', '*.tsx', '*.html'],
     },
     ...PluginImport.configs.typescript.settings,
-    "import/resolver": {
-      ...PluginImport.configs.typescript.settings["import/resolver"],
+    'import/resolver': {
+      ...PluginImport.configs.typescript.settings['import/resolver'],
       typescript: {
         alwaysTryTypes: true,
-        project: ["**/frontend/tsconfig.json", "**/backend/tsconfig.json"],
+        project: ['**/frontend/tsconfig.json', '**/backend/tsconfig.json'],
       },
     },
   },
