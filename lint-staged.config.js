599fb57e2b40250cb5c6c59dc2ce418746c1b2a4
Simon Stranks <sstranks87@gmail.com>
2026-07-06 13:33:52 +0100

    chore(lint-staged): eslint-perfectionist object order

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,8 +5,8 @@
 
 export default {
   '*': ['secretlint'],
-  '*.{js,ts,jsx,tsx}': ['eslint --max-warnings=0 --no-warn-ignored', 'prettier --check'],
-  '*.{graphql,html,html,json,jsonc,json5,yml,yaml}': ['eslint --no-warn-ignored', 'prettier --check'],
   '*.{css, scss, sass}': ['stylelint', 'prettier --check'],
+  '*.{graphql,html,html,json,jsonc,json5,yml,yaml}': ['eslint --no-warn-ignored', 'prettier --check'],
+  '*.{js,ts,jsx,tsx}': ['eslint --max-warnings=0 --no-warn-ignored', 'prettier --check'],
   '*.md': ['prettier --check'],
 };
