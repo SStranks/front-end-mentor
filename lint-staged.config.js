9151750738ab3b96e20edb2807e7954ef68ee675
Simon Stranks <sstranks87@gmail.com>
2026-07-11 13:40:06 +0100

    fix(lint-staged): typo fix

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,7 +5,7 @@
 
 export default {
   '*': ['secretlint'],
-  '*.{css, scss, sass}': ['stylelint', 'prettier --check'],
+  '*.{css,scss,sass}': ['stylelint', 'prettier --check'],
   '*.{graphql,html,html,json,jsonc,json5,yml,yaml}': ['eslint --no-warn-ignored', 'prettier --check'],
   '*.{js,ts,jsx,tsx}': ['eslint --max-warnings=0 --no-warn-ignored', 'prettier --check'],
   '*.md': ['prettier --check'],
